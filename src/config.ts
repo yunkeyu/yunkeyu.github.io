a5acd7bf569631f5180a3753197ae93edfe7d0da
yunkeyu <re0.init1@gmail.com>
2026-04-26 15:32:56 +0800

    chore: change site message

--- src/config.ts
+++ src/config.ts
@@ -54,8 +54,8 @@ export const navBarConfig: NavBarConfig = {
 
 export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-	name: "Lorem Ipsum",
-	bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+	name: "淬刃川",
+	bio: "孤阴不生，孤阳不长",
 	links: [
 		{
 			name: "Bilibili",
